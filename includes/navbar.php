f54f1a13b476c37cc5a074cdc8a750046e99a710
Kevin Riehl <kevinriehl@gmail.com>
2017-05-18 10:47:55 -0700

    Initial Commit

---
+++ includes/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li> <a href="index.html">Home</a></li>
+                <li> <a href="resume.html">R&eacute;sum&eacute;</a></li>
+                <li> <a href="contacts.php">Contacts</a></li>
+                <li> <a href="contact-form.php">Create Contact</a></li>
+                <li> <a href="references.html">References</a></li>
+            </ul>
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 12/5/16
+ * Time: 7:49 AM
+ */
